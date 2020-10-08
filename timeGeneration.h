cf5766b063c8872cd8007919567ccf03ad7353cc
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-10-08 10:40:16 +0200

    Update timeGeneration.h

--- timeGeneration.h
+++ timeGeneration.h
@@ -5,9 +5,12 @@
 #define MIN_TIME_DIZAINE  55
 #define MAX_TIME_CENTIEME  999
 #define MIN_TIME_CENTIEME  001
+#define MAX_TIME_STAND_DIZAINE 27
+#define MIN_TIME_STAND_DIZAINE 23
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-int getRandomTime();
+int getRandomTimeSector();
+int getRandomTimeStand();
 #endif //PROJET_C_TIMEGENERATION_H
