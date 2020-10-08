bbb81f126d4f29d0fc186caee7e4bc907cffa51b
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-10-08 10:29:15 +0200

    Update structure.h

--- structure.h
+++ structure.h
@@ -1,15 +1,20 @@
-//
-// Created by mathi on 08-10-20.
-//
 
 #ifndef PROJET_C_STRUCTURE_H
 #define PROJET_C_STRUCTURE_H
 
 #endif //PROJET_C_STRUCTURE_H
 
-
+typedef VoitureF1 VoitureF1;
 struct VoitureF1{
     int numeroVoiture;
-
-
+    int secteur1;
+    int secteur2;
+    int secteur3;
+    int bestTime;
+    int totalTime;
+    int nbreTours;
+    int status;
+    int etat;
+    int crashed;
+    int stand;
 };
