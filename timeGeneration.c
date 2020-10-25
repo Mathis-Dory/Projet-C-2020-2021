dfa194b8a9e06bb20400a882bb781ce1acf47366
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-10-25 15:53:08 +0100

    Update timeGeneration.c

--- timeGeneration.c
+++ timeGeneration.c
@@ -1,19 +1,38 @@
 
 #include "timeGeneration.h"
 
-int getRandomTimeSector(){
+int getRandomTimeSector() {
     double randomTimeTotal;
-    int randomTimeDizaine = rand() % (MAX_TIME_DIZAINE  + 1 - MIN_TIME_DIZAINE) + MIN_TIME_DIZAINE;
-    int randomTimeCentieme = rand() % (MAX_TIME_CENTIEME  + 1 - MIN_TIME_CENTIEME) + MIN_TIME_CENTIEME;
-    randomTimeTotal = (randomTimeCentieme/100.000) + randomTimeDizaine;
-    printf("Nombre: %.3f \n", randomTimeTotal);
+    int randomTimeDizaine = rand() % (MAX_TIME_DIZAINE + 1 - MIN_TIME_DIZAINE) + MIN_TIME_DIZAINE;
+    int randomTimeCentieme = rand() % (MAX_TIME_CENTIEME + 1 - MIN_TIME_CENTIEME) + MIN_TIME_CENTIEME;
+    randomTimeTotal = (randomTimeCentieme / 100.000) + randomTimeDizaine;
+    return randomTimeTotal;
 }
 
-int getRandomTimeStand(){
+int getRandomTimeStand() {
     double timeStand;
-    int timeStandDizaine= rand() % (MAX_TIME_STAND_DIZAINE + 1 - MIN_TIME_STAND_DIZAINE) + MIN_TIME_STAND_DIZAINE;
+    int timeStandDizaine = rand() % (MAX_TIME_STAND_DIZAINE + 1 - MIN_TIME_STAND_DIZAINE) + MIN_TIME_STAND_DIZAINE;
     int timeStandCentieme = rand() % (MAX_TIME_CENTIEME + 1 - MIN_TIME_CENTIEME) + MIN_TIME_CENTIEME;
-    timeStand = (timeStandCentieme/100.000) + timeStandDizaine;
-    printf("Temps au stand: %.3f \n", timeStand);
+    timeStand = (timeStandCentieme / 100.000) + timeStandDizaine;
+    return timeStand;
 
+}
+
+int carCrashed() {
+    int randomCrash = rand() % (100 + 1);
+    if (randomCrash <= CHANCE_DE_CRASH) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int isInStand(){
+    int randomStand = rand() % (100 + 1);
+    if (randomStand <= CHANCE_DE_STAND){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
