6aa4d769d087830f0fdfb97c5e60b705d1f1c584
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-10-25 15:53:40 +0100

    Update timeGeneration.h

--- timeGeneration.h
+++ timeGeneration.h
@@ -1,4 +1,3 @@
-
 #ifndef PROJET_C_TIMEGENERATION_H
 #define PROJET_C_TIMEGENERATION_H
 #define MAX_TIME_DIZAINE  35
@@ -7,10 +6,21 @@
 #define MIN_TIME_CENTIEME  001
 #define MAX_TIME_STAND_DIZAINE 27
 #define MIN_TIME_STAND_DIZAINE 23
+#define CHANCE_DE_CRASH 3 // chance en % d'avoir un crash
+#define CHANCE_DE_STAND 15 // chance en % d'aller au stand
+
+
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 int getRandomTimeSector();
+
 int getRandomTimeStand();
+
+int carCrashed();
+
+int isInStand();
+
 #endif //PROJET_C_TIMEGENERATION_H
