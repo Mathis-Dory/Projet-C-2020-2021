a44e139ff71261b468102e05df8ed2515d3a5c43
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-11-12 10:32:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include "circuit.h"
 
 
 int main(int argc, char *argv[]) {
-    getRandomTimeSector();
-    getRandomTimeStand();
+    tourEssai();
     return 0;
 }
