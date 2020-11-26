4f66b64460de1346ec4f8eefd4887290750af794
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-11-26 09:27:38 +0100

    Update circuit.c

--- circuit.c
+++ circuit.c
@@ -2,12 +2,10 @@
 #include "circuit.h"
 
 
-
-double turn(CarF1* car){
-    double total = 0; // Temps pour un tour
+double turn(CarF1 *car) {
+    double total = 0; // Temps pour un tour complet
     double timeSector = 0; // Temps pour un secteur
-    int i = 1;
-    while (i <= 3) {
+    for (int i = 1; i <= 3; ++i) {
         switch (i) {
             case 1:
                 timeSector = sector();
@@ -19,50 +17,38 @@ double turn(CarF1* car){
                 timeSector = sector();
                 break;
         }
-        if(timeSector == 0){                 //test si il y a un crash
-            //sem_wait(sem);
+        if (timeSector == 0) {                 //test si il y a un crash
             car->status = 0;
             car->crash = 1;
             car->ranking = 1;
+            printf("Il y a eu un crash");
             resetSector(car);
-            //sem_post(sem);
             return 0;
         }
-        if((i%2)==0){             //si il passe dans le secteur 2
-            //sem_wait(sem);
+        if ((i % 2) == 0) {             //si il passe dans le secteur 2
             car->sector2 = timeSector;
-            //sem_post(sem);
+
         }
-        else if((i%3)==0){        //si il passe dans le secteur 3
-            if(isInStand()){
+        else if ((i % 3) == 0) {        //si il passe dans le secteur 3
+            if (isInStand()) {
                 timeSector += getRandomTimeStand();
-
-                //sem_wait(sem);
-                car->status=1;
-                //sem_post(sem);
-
-                //sleep(tempsStand/100);         // endormir le processus pendant s*10 milliseconde
+                car->status = 1;
 
             }
-            //sem_wait(sem);
-            car->status=2;
+            car->status = 2;
             car->sector3 = timeSector;
-            //sem_post(sem);
-        }
-        else{                     //si il passe dans le secteur 1
-            //sem_wait(sem);
+
+        } else {                     //si il passe dans le secteur 1
             car->sector1 = timeSector;
-            //sem_post(sem);
         }
 
         total += timeSector; //ajout au temps total de la voiture dans le circuit
-        i ++;
     }
     printf("%f", total);
     return total;
 }
 
-void resetSector(CarF1* car){
+void resetSector(CarF1 *car) {
     car->sector1 = 0;
     car->sector2 = 0;
     car->sector3 = 0;
