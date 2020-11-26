f14514e2d4cfc4b1f44cd97b2383c7a5f6b4679b
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-11-26 10:38:38 +0100

    Update circuit.h

--- circuit.h
+++ circuit.h
@@ -1,6 +1,13 @@
 
+
 #ifndef MAIN_C_CIRCUIT_H
 #define MAIN_C_CIRCUIT_H
+#define CHRONO_E1 = 5400
+#define CHRONO_E2 = 5400
+#define CHRONO_E3 = 3600
+#define CHRONO_Q1 = 1080
+#define CHRONO_Q2 = 900
+#define CHRONO_Q3 = 720
 #include "timeGeneration.h"
 #include "structure.h"
 #include "secteur.h"
