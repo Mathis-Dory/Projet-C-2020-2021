b9ab18ebb930e387f5d9b8bd590e8ba2657fe27c
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-12-10 10:32:37 +0100

    Update structure.h

--- structure.h
+++ structure.h
@@ -11,10 +11,11 @@ typedef struct {
     int bestTime;
     int totalTime;
     int nbreTurns;
-    int status;
-    int ranking;
-    int crash;
-    int stand;
+    int status; // 0 = Crash 1 = En course 2 = stand -1 = OUT
+    int ranking; // Place dans le classement
+    int crash; // 1 pour True 0 pour False
+    int stand; // 1 pour True 0 pour False
+    int updateRanking; // 1 True 0 False
 }CarF1;
 
 #endif //PROJET_C_STRUCTURE_H
